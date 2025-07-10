fc7b6d8af8c8cec8988567c097c5edbf9d789c92
mrishan843 <mrishan843@gmail.com>
2025-07-10 21:54:46 +0530

    more changes

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal(".home-content p , .about-content", { origin: "right" });
 
 // typed js
 const typed = new Typed(".multiple-text", {
-  strings: ["Software developer", "Btech graduate"],
+  strings: ["Software developer", "B.Tech graduate"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
